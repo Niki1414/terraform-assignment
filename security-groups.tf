d8af346cd0d78f95304d8e8c96eca9b23f589cb0
Niki1414 <49054569+Niki1414@users.noreply.github.com>
2021-01-27 15:15:39 +0530

    Create security-groups.tf

---
+++ security-groups.tf
@@ -0,0 +1,38 @@
+resource "aws_security_group" "webservers" {
+  name        = "allow_http"
+  description = "Allow http inbound traffic"
+  vpc_id      = "${aws_vpc.terra_vpc.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private_webserverss" {
+  name        = "allow_http_elb"
+  description = "Allow http inbound traffic frpm elb"
+  vpc_id      = "${aws_vpc.terra_vpc.id}"
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+
+    security_groups = ["${aws_security_group.webservers.id}"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
