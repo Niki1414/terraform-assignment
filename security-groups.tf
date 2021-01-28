df8952959d54bc5a5faccdccf60062ba21e5fd95
Niki1414 <49054569+Niki1414@users.noreply.github.com>
2021-01-28 12:38:46 +0530

    Update security-groups.tf

--- security-groups.tf
+++ security-groups.tf
@@ -29,6 +29,31 @@ resource "aws_security_group" "private_webserverss" {
 
     security_groups = ["${aws_security_group.webservers.id}"]
   }
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.20.0.0/16"]
+
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "bastion_sec_group" {
+  name        = "allow_ssh_from_everywhere"
+  description = "Allow http inbound traffic frpm elb"
+  vpc_id      = "${aws_vpc.terra_vpc.id}"
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
   egress {
     from_port   = 0
     to_port     = 0
