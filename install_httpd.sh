880bae2f77e9dc8e168c647ea37cd0e58e5a6c98
Niki1414 <49054569+Niki1414@users.noreply.github.com>
2021-01-28 12:55:43 +0530

    Update install_httpd.sh

--- install_httpd.sh
+++ install_httpd.sh
@@ -4,3 +4,6 @@ sudo yum install -y httpd
 sudo chkconfig httpd on
 sudo service httpd start
 echo "<h1>Deployed via Terraform wih ELB</h1>" | sudo tee /var/www/html/index.html
+sudo mkfs.ext4 /dev/xvdf
+sudo mount /dev/xvdf /var/log
+echo '/dev/xvdf /database ext4 defaults 0 0' >> /etc/fstab
